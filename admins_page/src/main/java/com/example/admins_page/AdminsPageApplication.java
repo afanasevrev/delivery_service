7961af4cd2b44a1b7f9b5409c96cfd09e32c7115
revoly afanasev <rewoly@mail.ru>
2024-06-16 01:11:09 +0900

    for student

--- admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
+++ admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
@@ -1,13 +1,13 @@
 package com.example.admins_page;
 
+import com.example.admins_page.entity.Admins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminsPageApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdminsPageApplication.class, args);
+		Admins admins = new Admins();
 	}
-
 }
--- admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -1,8 +1,9 @@
 package com.example.admins_page.controllers;
 
 import org.springframework.stereotype.Controller;
-
 @Controller
 public class WebController {
-
+    private String getMainPage() {
+        return "admins_page";
+    }
 }
