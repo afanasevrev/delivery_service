d251c7779a8f71d511175639e5e4453b39b9a364
revoly afanasev <rewoly@mail.ru>
2024-06-16 01:23:21 +0900

    for student

--- admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -1,8 +1,11 @@
 package com.example.admins_page.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class WebController {
+    @GetMapping("/")
     private String getMainPage() {
         return "admins_page";
     }
