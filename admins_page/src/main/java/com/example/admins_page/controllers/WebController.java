ca631f1e8fd8d4f89b1ad21325af98fe34d16b9c
revoly afanasev <rewoly@mail.ru>
2024-06-16 01:04:51 +0900

    for student

---
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -0,0 +1,8 @@
+package com.example.admins_page.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+
+}
---
+++ admins_page/src/main/java/com/example/admins_page/entity/Admins.java
@@ -0,0 +1,25 @@
+package com.example.admins_page.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "admins")
+public class Admins {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    public Admins() {}
+    public Admins(Long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+}
