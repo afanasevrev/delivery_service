40b142cbf6940367f54c64714f96ee6f61356ef9
revoly afanasev <rewoly@mail.ru>
2024-06-17 00:34:31 +0900

    for student

--- admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
+++ admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
@@ -1,11 +1,18 @@
 package com.example.admins_page;
 
 import com.example.admins_page.entity.Admins;
+import com.example.admins_page.service.AdminsService;
+import com.example.admins_page.service.AdminsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdminsPageApplication {
+	@Bean
+	public AdminsService adminsService() {
+		return new AdminsServiceImpl();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AdminsPageApplication.class, args);
 		Admins admins = new Admins();
--- admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -2,23 +2,22 @@ package com.example.admins_page.controllers;
 
 import com.example.admins_page.entity.Admins;
 import com.example.admins_page.service.AdminsService;
-import com.example.admins_page.service.AdminsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    private AdminsService adminsService;
+    public WebController() {}
     @Autowired
-    private final AdminsService adminsService;
     public WebController(AdminsService adminsService) {
         this.adminsService = adminsService;
     }
     @GetMapping("/")
     private String getMainPage() {
         Admins admin = new Admins("Topsy Gilligan", "123qweASD");
-        AdminsServiceImpl adminsServiceImpl = new AdminsServiceImpl();
-        adminsServiceImpl.createAdmin(admin);
+        adminsService.createAdmin(admin);
         return "admins_page";
     }
 }
