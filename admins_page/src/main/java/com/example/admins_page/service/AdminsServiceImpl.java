5ed7740f53ebd5d3a835360b394950bdaca7ddab
revoly afanasev <rewoly@mail.ru>
2024-06-16 23:47:58 +0900

    for student

---
+++ admins_page/src/main/java/com/example/admins_page/service/AdminsServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.admins_page.service;
+
+import com.example.admins_page.entity.Admins;
+import com.example.admins_page.repository.AdminsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AdminsServiceImpl implements AdminsService{
+    @Autowired
+    private AdminsRepository adminsRepository;
+    @Override
+    public String createAdmin(Admins admin) {
+        adminsRepository.save(admin);
+        return "Новый администратор добавлен в БД";
+    }
+}
