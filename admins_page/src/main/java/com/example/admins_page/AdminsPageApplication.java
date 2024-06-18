74615a95ad6946a35ce9ece757fb650e121576d2
revoly afanasev <rewoly@mail.ru>
2024-06-19 00:49:29 +0900

    for student

--- admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
+++ admins_page/src/main/java/com/example/admins_page/AdminsPageApplication.java
@@ -6,6 +6,9 @@ import com.example.admins_page.service.AdminsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+/**
+ * Точка запуска приложения
+ */
 @SpringBootApplication
 public class AdminsPageApplication {
 	@Bean
--- admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -1,9 +1,14 @@
 package com.example.admins_page.controllers;
 
+import com.example.admins_page.entity.AdminsEntity;
 import com.example.admins_page.service.AdminsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 @Controller
 public class WebController {
     private AdminsService adminsService;
@@ -16,5 +21,9 @@ public class WebController {
     private String getMainPage() {
         return "admins_page";
     }
-
+    @GetMapping("/delete/{id}")
+    private String deleteAdmin(@PathVariable Long id) {
+        adminsService.deleteAdmin(id);
+        return "admins_page";
+    }
 }
