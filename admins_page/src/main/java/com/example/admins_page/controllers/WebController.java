782bdcb57bc1df1a60099885e9c738a8fad3766c
revoly afanasev <rewoly@mail.ru>
2024-06-19 23:57:37 +0900

    for student

--- admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
+++ admins_page/src/main/java/com/example/admins_page/controllers/WebController.java
@@ -1,14 +1,13 @@
 package com.example.admins_page.controllers;
 
-import com.example.admins_page.entity.AdminsEntity;
 import com.example.admins_page.service.AdminsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+/**
+ * Контроллер - отвечает на запросы от клиентов
+ */
 @Controller
 public class WebController {
     private AdminsService adminsService;
